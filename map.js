c1999c1c0ea2dbf5269d169e547dc767959f5040
jjhi11 <jayhill@utah.gov>
2020-03-26 13:57:00 -0600

    test

--- map.js
+++ map.js
@@ -60,11 +60,7 @@ require([
     "dojo/domReady!"
 ], function(Map, MapView, SimpleMarkerSymbol, GraphicsLayer, SketchViewModel, Graphic, FeatureLayer, MapImageLayer, Query, QueryTask, Home, ScaleBar, Zoom, Compass, Search, Legend, Expand, LayerList, BasemapToggle, watchUtils, RelationshipQuery, AttachmentsContent, Collapse, Dropdown, query, Memory, ObjectStore, ItemFileReadStore, DataGrid, OnDemandGrid, ColumnHider, Selection, StoreAdapter, List, declare, parser, aspect, request, mouse, CalciteMaps, CalciteMapArcGISSupport, on, arrayUtils, dom, domClass, domConstruct) {
 
-    /******************************************************************
-     *
-     * Create the map, view and widgets
-     * 
-     ******************************************************************/
+    //Create the map, view and widgets
 
     let grid;
 
